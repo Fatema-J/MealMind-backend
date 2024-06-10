836909eb9b8a6314c57280bfb68af928e0f214a4
Fatem J <fatima.aljunaid.257@gmail.com>
2024-06-10 20:35:18 +0300

    initial express app structure

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+
+const PORT = process.env.PORT || 3001
+
+const db = require('./db')
+
+const app = express()
+
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/', (req, res) => {
+  res.send(`Connected!`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Running Express server on Port ${PORT} . . .`)
+})
