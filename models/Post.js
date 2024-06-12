01313957ce43a7d149d9866db74b8936a54c337d
Fatem J <fatima.aljunaid.257@gmail.com>
2024-06-12 12:59:28 +0300

    added comments reference field in post model

--- models/Post.js
+++ models/Post.js
@@ -1,10 +1,12 @@
 const { Schema } = require('mongoose')
+const commentSchema = require('./Comment')
 
 const postSchema = new Schema(
   {
     user: { type: Schema.Types.ObjectId, ref: 'User' },
     title: { type: String, required: true },
-    caption: { type: String }
+    caption: { type: String },
+    comments: [commentSchema]
   },
   { timestamps: true }
 )
